54e341deb402fc4723658e9406c8247c997944a7
Brandon Schwanzl <bs383722@ohio.edu>
2023-09-08 15:46:02 -0400

    lab program commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Brandon Schwanzl
+ *        Date: September 8th, 2023
+ * Description: Prints, "Hello Git!"
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!" << endl;
+    return 0;
+}// main
